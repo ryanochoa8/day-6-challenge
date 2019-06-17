61299f1e59e8d8223c7bc0f4499a467f4fdeb28b
ryanochoa8 <ryan.ochoa888@gmail.com>
2019-06-16 22:39:30 -0600

    commit

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ let snack = {
 }
 
 
-
+// breaker breaker breaker
 
 let fruit = "apple"
 let orangeCount = 0
@@ -50,7 +50,6 @@ switch (fruit) {
     console.log("You found an orange!")
     break;
   case "apple":
-    debugger
     console.log("You found an Apple!")
     break;
   case "banana":
@@ -58,4 +57,118 @@ switch (fruit) {
     break;
   default:
     console.log("you found an unknown fruit!")
+}
+
+// breaker breaker breaker
+
+let count = 1
+
+while (count <= 10) {
+  console.log(count)
+  count++
+}
+
+// breaker breaker breaker
+
+// let the user interact with the function
+
+function sumOfTwoNumbers(num1, num2) {
+  console.log(num1 + num2)
+}
+
+let number1 = prompt("You are adding two numbers. What is your first number?")
+let number2 = prompt("What is your second number?")
+
+sumOfTwoNumbers(number1, number2)
+
+
+// breaker breaker breaker
+
+
+let numbers = [23, 52, 67, 91, 200]
+
+
+for (let i = 0; i < numbers.length; i++) {
+  console.log(numbers[i])
+}
+
+
+
+// breaker breaker breaker
+
+
+
+let people = [
+  {
+    name: "Mark"
+  },
+  {
+    myName: "Ryan"
+  },
+  {
+    theirName: "Kenny"
+  }
+]
+
+for (let i = 0; i < people.length; i++) {
+  console.log(people[i])
+}
+
+
+
+
+// breaker breaker breaker
+
+let people = [
+  {
+    name: "Mark"
+  },
+  {
+    name: "Ryan"
+  },
+  {
+    name: "Kenny"
+  }
+]
+
+
+for (let index = 0; index < people.length; index++) {
+  let person = people[index]
+  console.log(person.name)
+}
+
+// THIS IS CALLED ITERATION OR ITERATING
+// breaker breaker breaker breaker breaker breaker
+
+
+
+
+
+
+let people = [
+  {
+    name: "Mark"
+  },
+  {
+    name: "Ryan"
+  },
+  {
+    name: "Kenny"
+  }
+]
+
+let newPerson = {
+  name: "Jake"
+}
+let Brittany = {
+  name: "Brittany"
+}
+people.push(newPerson)
+people.push({ name: "Zach" })
+
+
+
+for (let index = 0; index < people.length; index++) {
+  let person = people[index]
+  console.log(person.name)
 }
